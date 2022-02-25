0d3d91761b889d023e7117343c894d687c26ca1e
RainForDays <sebb.tornquist@gmail.com>
2022-02-25 19:52:20 +0100

    fixup new character component

--- src/components/NewCharacter/NewCharacter.js
+++ src/components/NewCharacter/NewCharacter.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../Button';
+import Modal from '../Modal';
 
-const NewCharacter = props => {
+const modalFields = [
+  {
+    type: 'text',
+    name: 'name',
+    // placeholder: 'Character name',
+    label: 'Character name',
+  },
+];
+
+const NewCharacter = ({ className }) => {
+  const [modal, showModal] = useState(false);
+
   return (
-    <div>
-      <Button color="green" size="md">
-        Add Character
-      </Button>
+    <div className={className}>
+      {modal && <Modal fields={modalFields} onClose={() => showModal(false)} />}
+      <Button
+        color="green"
+        size="md"
+        onClick={() => showModal(true)}
+        label="New character"
+      />
     </div>
   );
 };
