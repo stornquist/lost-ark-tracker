0a31e26b1ac5dc14ca647d6b0d7fe23866116b79
RainForDays <sebb.tornquist@gmail.com>
2022-02-25 18:17:22 +0100

    new character component

---
+++ src/components/NewCharacter/NewCharacter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '../Button';
+
+const NewCharacter = props => {
+  return (
+    <div>
+      <Button color="green" size="md">
+        Add Character
+      </Button>
+    </div>
+  );
+};
+
+export default NewCharacter;
