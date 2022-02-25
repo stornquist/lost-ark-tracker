c02c94de8a58cc3de6f699ffaa5e62e23c18b6ac
RainForDays <sebb.tornquist@gmail.com>
2022-02-25 19:51:36 +0100

    add guild donate as default

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       id: 1,
       name: 'Test',
       tasks: {
+        'guild donate': { type: 'daily', completed: false },
         chaos: { type: 'daily', completed: false },
         guardian: { type: 'daily', completed: false },
         abyss: { type: 'weekly', completed: false },
