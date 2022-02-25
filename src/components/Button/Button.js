250d5e4b0b9943f2d0dd8b5915a0c57dca09fb15
RainForDays <sebb.tornquist@gmail.com>
2022-02-25 19:51:43 +0100

    fixup button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import cn from 'classnames';
 
-const Button = ({ size, onClick, children, color }) => {
-  const paddingMap = {
-    sm: 1,
-    md: 2,
-    lg: 3,
-  };
+const paddingMap = {
+  sm: 1,
+  md: 2,
+  lg: 3,
+};
+
+const Button = ({ size, onClick, label, color, className }) => {
   const classes = cn(
-    'mb-4 rounded border saturate-50 hover:saturate-100 ',
+    'mb-4 rounded border saturate-50 hover:saturate-100',
+    className,
     {
       'bg-purple-400 border-purple-500': !color,
       'bg-green-600 border-green-700': color === 'green',
       'bg-red-600 border-red-700': color === 'red',
-      [`p-${paddingMap[size]}`]: true,
+      [`p-${paddingMap[size]}`]: size,
     }
-    // 'bg-gray-500 border-gray-600'
   );
-  return <button className={classes}>{children}</button>;
+
+  return (
+    <button onClick={onClick} className={classes}>
+      {label}
+    </button>
+  );
 };
 
 export default Button;
