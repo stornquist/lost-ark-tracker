e6db4447045f507892424b344ceb808e378bbfa7
RainForDays <sebb.tornquist@gmail.com>
2022-02-25 18:18:39 +0100

    use new character component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import DataTable, { createTheme } from 'react-data-table-component';
+import NewCharacter from '../NewCharacter';
 import { useMainColumns } from './hooks/columns';
 
 createTheme('custom', {
@@ -22,6 +23,7 @@ const Main = ({ data, setData }) => {
   const columns = useMainColumns({ data, handleTaskComplete });
   return (
     <div className="m-32">
+      <NewCharacter />
       <DataTable data={data} columns={columns} theme="custom" />
     </div>
   );
