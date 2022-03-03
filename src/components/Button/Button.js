5160a642bfbd5fbbd3204e4424a5bed14b85b80f
RainForDays <sebb.tornquist@gmail.com>
2022-03-03 23:57:28 +0100

    fix(Button): fix classes and props

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import cn from 'classnames';
 
-const paddingMap = {
-  sm: 1,
-  md: 2,
-  lg: 3,
-};
-
-const Button = ({ size, onClick, label, color, className }) => {
+const Button = ({ size, onClick, label, color, className, type, ...rest }) => {
   const classes = cn(
-    'mb-4 rounded border saturate-50 hover:saturate-100',
+    'mb-4 p-2 rounded border saturate-50 hover:saturate-100',
     className,
     {
       'bg-purple-400 border-purple-500': !color,
       'bg-green-600 border-green-700': color === 'green',
       'bg-red-600 border-red-700': color === 'red',
-      [`p-${paddingMap[size]}`]: size,
+      'p-1': size === 'sm',
+      'p-3': size === 'lg',
     }
   );
 
   return (
-    <button onClick={onClick} className={classes}>
+    <button
+      onClick={onClick ? onClick : undefined}
+      className={classes}
+      type={type ? type : 'button'}
+      {...rest}
+    >
       {label}
     </button>
   );
