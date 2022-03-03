344b2d2d84bb8a98f3edaf9c2d5e6684a663c8f6
RainForDays <sebb.tornquist@gmail.com>
2022-03-04 00:00:05 +0100

    fix(columns): cleanup and fix design

--- src/components/Main/hooks/columns.js
+++ src/components/Main/hooks/columns.js
@@ -2,27 +2,39 @@ import { useMemo } from 'react';
 import { capitalize } from '../../../utils/capitalize';
 import { BiTrash } from 'react-icons/bi';
 
-export const useMainColumns = ({ data, handleTaskComplete: setTaskComplete }) =>
+export const useCharactersColumns = ({
+  data,
+  handleTaskStatusChange: setTaskStatus,
+  onDeleteCharacter,
+}) =>
   useMemo(
     () => [
       {
         name: 'Character',
         selector: row => (
           <div className="group w-full">
-            {row.name}{' '}
-            <BiTrash
-              className="w-12 inline text-red-600 opacity-0 group-hover:opacity-100 transition-opacity"
-              size={18}
-            />
+            {row.name}
+            {data.length > 1 && (
+              <BiTrash
+                className="w-12 inline text-red-600 opacity-0 group-hover:opacity-100 transition-opacity"
+                size={18}
+                onClick={() => onDeleteCharacter(row)}
+              />
+            )}
           </div>
         ),
       },
-      ...Object.entries(data[0].tasks).map(([taskName, taskData]) => {
+      ...Object.entries(data[0].tasks).map(([taskName, taskData], index) => {
         return {
+          id: 'taskname' + index,
           name: (
-            <div>
+            <div key={taskName + taskData.type}>
               {capitalize(taskName)}
-              {taskData.type ? ` (${taskData.type})` : ''}
+              <p>
+                {taskData.type
+                  ? ` (${taskData.type.charAt(0).toUpperCase()})`
+                  : ''}
+              </p>
             </div>
           ),
           selector: row => {
@@ -32,7 +44,7 @@ export const useMainColumns = ({ data, handleTaskComplete: setTaskComplete }) =>
                 className="appearance-none w-4 h-4 bg-red-400 checked:bg-green-400 rounded-sm"
                 checked={row.tasks[taskName].completed}
                 onChange={() =>
-                  setTaskComplete(
+                  setTaskStatus(
                     row.id,
                     taskName,
                     !row.tasks[taskName].completed
@@ -44,5 +56,5 @@ export const useMainColumns = ({ data, handleTaskComplete: setTaskComplete }) =>
         };
       }),
     ],
-    [(data, setTaskComplete)]
+    [data, setTaskStatus, onDeleteCharacter]
   );
