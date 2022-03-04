94789916cde910701886d42fa6759014aba9c0be
RainForDays <sebb.tornquist@gmail.com>
2022-03-04 12:25:45 +0100

    fix(App): cleanup date handling & fix hours logic

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { subDays } from 'date-fns';
 import { format } from 'date-fns/esm';
 import { useEffect } from 'react';
 import Main from './components/Main';
@@ -53,18 +54,12 @@ function App() {
     const now = new Date();
     const time = now.getUTCHours();
     const date = format(now, 'yyyy-MM-dd');
-    const aDayAgo = format(
-      new Date(now.getTime() - 1000 * 60 * 60 * 24),
-      'yyyy-MM-dd'
-    );
-    const aWeekAgo = format(
-      new Date(now.getTime() - 1000 * 60 * 60 * 24 * 7),
-      'yyyy-MM-dd'
-    );
+    const aDayAgo = format(subDays(now, 1), 'yyyy-MM-dd');
+    const aWeekAgo = format(subDays(now, 7), 'yyyy-MM-dd');
     if (!lastDailyReset) setDailyReset(aDayAgo);
     if (!lastWeeklyReset) setWeeklyReset(aWeekAgo);
 
-    if (time > 10 && lastDailyReset !== date) {
+    if (time >= 10 && lastDailyReset !== date) {
       resetDailyTasks();
       setDailyReset(date);
     }
