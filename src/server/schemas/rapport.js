5fab5b16671a647c0a13efa201d020934f73f84e
RainForDays <sebb.tornquist@gmail.com>
2022-03-15 11:23:41 +0100

    fix(Rapport): update schema with constraints

--- src/server/schemas/rapport.js
+++ src/server/schemas/rapport.js
@@ -2,6 +2,6 @@ import j from 'joi';
 
 export const rapportSchema = j.object({
   name: j.string().required(),
-  instrument: j.number().default(0),
-  emote: j.number().default(0),
+  instrument: j.number().default(0).min(0).max(2),
+  emote: j.number().default(0).min(0).max(2),
 });
